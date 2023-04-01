c66a9a67a0243759372b78ecc7ed4741fde8e97c
Eliot Alderson <e.alderson@allsafe.com>
2023-04-01 18:28:13 +0530

    Rebasing Done! Added a Readme file

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Doubt-Submission
-A Simple Doubt Submission Software made using flask
+A Simple Doubt Submission Software made using flask.
+
+A Simple Doubt Submmission application that notifies me, that someone has posted a doubt via 'notify-send' commans in Linux.
+Note: This feature might break the app on several systems so please remove the 'notify_send' function and its calls if this dependency can't be fulfilled.
+
+[https://github.com/vaskovsky/notify-send](Click here to install the Dependency on Windows)
+For Mac OS:
+Please the Subprocess command should be this 
+```bash
+osascript -e 'display notification "hello world!"'
+```
+
+So the line 41 after change will be something like this :
+```python
+p = subprocess.Popen(['osascript', '-e', '\'display', 'notification', f'"{title}', f'{message}"\''])
+```
+
+## Setup
+Clone this Repo then open a Terminal in that Path and run these commands.
+```bash
+python3 -m venv env
+source env/source/activate
+pip3 install -r requirements.txt
+```
+To run:
+```bash
+python3 app.py
+```
+
+## Workings
+- The Database is stored in instance/ after you run the app.py script.
+- The App has a API Endpoint '/api/doubts'. Make a GET Request and get results.
+- Everytime someone submits their doubt, it will run 'notify-send' command. If it is not present in your system, you can either install it (like in Mac and Linux) or just Disable the function and the calls (like in Windows) to Avoid any Potential errors. Without this Edit it might not run on many systems other than mine, since I have not tested the repo in windows or mac.
---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import subprocess
+import uuid
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///doubts.db'
+db = SQLAlchemy(app)
+
+class Doubt(db.Model):
+    id = db.Column(db.String(36), primary_key=True, default=str(uuid.uuid4()), unique=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    doubt = db.Column(db.String(500), nullable=False)
+
+    def __repr__(self):
+        return f"<Doubt {self.id}>"
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        doubt = request.form['doubt']
+        print(
+            f"""
+                {name}
+                {email}
+                {doubt}
+            """
+        )
+        d = notify_send(doubt, f"New Doubt by {name}")
+        new_doubt = Doubt(id=str(uuid.uuid4()),name=name, email=email, doubt=doubt)
+        db.session.add(new_doubt)
+        db.session.commit()
+        return redirect(url_for('index'))
+    else:
+        return render_template('index.html')
+
+def notify_send(message, title="New Doubt"):
+    p = subprocess.Popen(['notify-send', title, message])
+    return True, p
+
+@app.route('/api/doubts', methods=['GET'])
+def get_all_doubts():
+    doubts = Doubt.query.all()
+    result = []
+    for doubt in doubts:
+        doubt_data = {
+            'id': doubt.id,
+            'name': doubt.name,
+            'email': doubt.email,
+            'doubt': doubt.doubt
+        }
+        result.append(doubt_data)
+    return jsonify(result)
+
+if __name__ == '__main__':
+    with app.app_context():db.create_all()
+    app.run(debug=True)
+
